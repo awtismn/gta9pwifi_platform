5e0c55b7f392696d761015110649027c2c90bd2f
awtismn <awtismn@gmail.com>
2026-06-12 20:28:51 -0500

    Update build_64bit.sh

--- build_64bit.sh
+++ build_64bit.sh
@@ -21,10 +21,6 @@ echo
 echo 'Build android platform'
 echo
 
-echo "source build/envsetup.sh"
-source build/envsetup.sh
-lunch aosp_arm64-user
-
 make -j$CPU_JOB_NUM ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- arch/arm64/configs/vendor/gta9pwifi_eur_open_defconfig
 make update-api
 make -j$CPU_JOB_NUM ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu-
